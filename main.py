380a811fd1bbaa02d24ffebc2577b5eef85b8329
francesco <francescosacco.github@gmail.com>
2022-02-01 23:03:19 +0100

    Implemented the App Lifecycle

--- main.py
+++ main.py
@@ -6,4 +6,15 @@ import time
 # Defining the main program
 bot = Bot()
 if __name__ == "__main__":
-    pass
+    while True:
+        now = datetime.now()
+        h = now.strftime("%H")
+        m = now.strftime("%M")
+        print(h, ":", m)
+        time.sleep(1)
+        if h == bot.HOUR and m == bot.MINUTE:
+            bot.open_supreme_shop()
+            bot.ItemIterator()
+            bot.add_to_basket()
+            bot.go_checkout()
+            bot.Pay()
