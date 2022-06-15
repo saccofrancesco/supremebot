dfaabc2cea8a68ad169ed21117f258e69e8708e9
francesco <francescosacco.github@gmail.com>
2022-06-15 16:06:45 +0200

    Refactoring Changes

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from rich.console import Console
 from rich.progress import track
 from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup
-import requests
-import json
 from datetime import datetime
 from sys import exit
+import requests
+import json
 
-# Bot Class
+# Creating the Bot Class
 
 
 class Bot:
@@ -17,83 +17,109 @@ class Bot:
     def __init__(self) -> None:
 
         # Creating a Console Instance
-        self.console = Console()
+        self.CONSOLE = Console()
 
-        # Saving Items
+        # Opening the Items File
         with open("Items.json", "r") as i:
-            items_dict = json.load(i)
-            self.items_name = []
-            self.items_style = []
-            self.items_size = []
-            self.items_type = []
-            for item in items_dict:
-                self.items_name.append(item["Item"])
-                self.items_style.append(item["Style"])
-                self.items_size.append(item["Size"])
-                self.items_type.append(item["Type"])
 
-        # Saving Data
+            # Loading the JSON File
+            ITEMS = json.load(i)
+
+            # Creating the Lists
+            self.ITEMS_NAMES = []
+            self.ITEMS_STYLES = []
+            self.ITEMS_SIZES = []
+            self.ITEMS_TYPES = []
+
+            # Looping the JSON File
+            for item in ITEMS:
+
+                # Appending the Prameters to the Lists
+                self.ITEMS_NAMES.append(item["Item"])
+                self.ITEMS_STYLES.append(item["Style"])
+                self.ITEMS_SIZES.append(item["Size"])
+                self.ITEMS_TYPES.append(item["Type"])
+
+        # Opening the Data File
         with open("Data.json", "r") as d:
-            datas_dict = json.load(d)
-            for data in datas_dict:
-                self.name_surname = data["Name Surname"]
-                self.email = data["Email"]
-                self.tel = data["Tel"]
-                self.address = data["Address"]
-                self.city = data["City"]
-                self.country = data["Country"]
-                self.postal_code = data["Postal Code"]
-                self.card_number = data["Card Number"]
-                self.month_exp = data["Expiration Month"]
-                self.year_exp = data["Expiration Year"]
-                self.cvv = data["CVV"]
 
-        # Item's Links Listing
+            # Loading the JSON File
+            DATA = json.load(d)
+
+            # Looping the JSON File
+            for info in DATA:
+
+                # Storing Personal Data
+                self.NAME_SURNAME = info["Name Surname"]
+                self.EMAIL = info["Email"]
+                self.TEL = info["Tel"]
+                self.ADDRESS = info["Address"]
+                self.CITY = info["City"]
+                self.COUNTRY = info["Country"]
+                self.POSTAL_CODE = info["Postal Code"]
+                self.CARD_NUMBER = info["Card Number"]
+                self.MONTH_EXP = info["Expiration Month"]
+                self.YEAR_EXP = info["Expiration Year"]
+                self.CVV = info["CVV"]
+
+        # Creating the Link's List
         self.links_list = []
 
         # Buy Times
         self.HOUR = "12"
         self.MINUTE = "0"
 
-    # Scrape Method for Saving the urls
+    # Scrape Method for Saving the URLs
     def scrape(self) -> None:
 
         # Looping the Items' Types
-        for i in track(range(len(self.items_name)),
+        for i in track(range(len(self.ITEMS_NAMES)),
                        description="🔗 [blue]Extracting Items' Links...[/blue]"):
 
-            # Scraping
+            # URL to Scrape
             url = "https://www.supremenewyork.com/shop/all/" + \
-                self.items_type[i]
+                self.ITEMS_TYPES[i]
+
+            # Requesting the Page
             source = requests.get(url).text
             soup = BeautifulSoup(source, "html.parser")
+
+            # Storing all the Links of the Page
             links = [
                 "https://www.supremenewyork.com" + link["href"]
                 for link in soup.find_all("a", class_="name-link")
             ]
 
-            # Removing Duplicates
+            # Removing Duplicates Links from the List
             links = list(dict.fromkeys(links))
 
             # Checking for the Right Link
-            for link_ in links:
-                source = requests.get(link_).text
+            for link in links:
+
+                # Requesting the Link's page
+                source = requests.get(link).text
                 soup = BeautifulSoup(source, "html.parser")
+
+                # Finding the name and Style of the Item
                 name = soup.find("h1", class_="protect").text
                 style = soup.find("p", class_="style protect").text
-                if name == self.items_name[i] and style == self.items_style[i]:
-                    self.links_list.append(link_)
+
+                # Checking if the Item is To Buy
+                if name == self.ITEMS_NAMES[i] and style == self.ITEMS_STYLES[i]:
+
+                    # Appending the Link to the List of Links to Buy
+                    self.links_list.append(link)
                     break
 
-    # Method for add to the cart the requeted elements
-    def addTobasket(self, page) -> None:
+    # Method for Add to the Cart the requested Items
+    def add_to_basket(self, page) -> None:
 
         # Looping the Element's to Buy List
         for i in track(range(len(self.links_list)),
                        description="💸 [green]Buying the Items...[/green]"):
             page.goto(self.links_list[i])
 
-            # Saving Page Data
+            # Requesting the Item's Page
             source = requests.get(self.links_list[i]).text
             soup = BeautifulSoup(source, "html.parser")
 
@@ -103,70 +129,79 @@ class Bot:
                     id="size") and soup.find(
                     "input",
                     type="submit"):
+
+                # Selecting the Size
                 page.click("#size")
                 option = page.query_selector("#size")
-                option.select_option(label=self.items_size[i])
+                option.select_option(label=self.ITEMS_SIZES[i])
+
+                # Adding the Item to the Cart
                 page.click("input.button")
 
     # Method for Compiling the Checkout Form
     def checkout(self, page) -> None:
 
-        # Performing Actions while in an Animation
-        with self.console.status("🖋️ [yellow]Performing the Checkout...[/yellow]"):
+        # Performing Actions during an Animation
+        with self.CONSOLE.status("🖋️ [yellow]Performing the Checkout...[/yellow]"):
 
             # Going to the Checkout
             page.click("a.button:nth-child(3)")
 
             # Using Data to Compile the Form
-            page.fill("#order_billing_name", self.name_surname)
-            page.fill("#order_email", self.email)
-            page.fill("#order_tel", self.tel)
-            page.fill("#order_billing_address", self.address)
-            page.fill("#order_billing_city", self.city)
-            page.fill("#order_billing_zip", str(self.postal_code))
+            # Name, Surname, Email and Tel
+            page.fill("#order_billing_name", self.NAME_SURNAME)
+            page.fill("#order_email", self.EMAIL)
+            page.fill("#order_tel", self.TEL)
+
+            # Address, City, Country and Postal Code
+            page.fill("#order_billing_address", self.ADDRESS)
+            page.fill("#order_billing_city", self.CITY)
+            page.fill("#order_billing_zip", str(self.POSTAL_CODE))
             page.click("#order_billing_country")
             option = page.query_selector("#order_billing_country")
-            option.select_option(label=self.country.upper())
-            page.fill("#credit_card_number", str(self.card_number))
+            option.select_option(label=self.COUNTRY.upper())
+
+            # Card Number, Expiration Date and CVV
+            page.fill("#credit_card_number", str(self.CARD_NUMBER))
             page.click("#credit_card_month")
             option = page.query_selector("#credit_card_month")
-            option.select_option(label=str(self.month_exp))
+            option.select_option(label=str(self.MONTH_EXP))
             page.click("#credit_card_year")
             option = page.query_selector("#credit_card_year")
-            option.select_option(label=str(self.year_exp))
-            page.fill("#credit_card_verification_value", str(self.cvv))
+            option.select_option(label=str(self.YEAR_EXP))
+            page.fill("#credit_card_verification_value", str(self.CVV))
 
 
 # Main Program
 if __name__ == "__main__":
 
-    # Creating a Bot Instance
-    bot = Bot()
+    # Creating the Bot Instance
+    BOT = Bot()
 
-    # Create a Loop for Instants Buying
+    # Create a Loop for Instant Buying
     while True:
 
         # Saving the Current Time
         now = datetime.now()
 
-        # Checking if the Curretn Time == the Time of the Drop
-        if str(now.hour) == bot.HOUR and str(now.minute) == bot.MINUTE:
+        # Checking if the Current Time == the Time of the Drop
+        if str(now.hour) == BOT.HOUR and str(now.minute) == BOT.MINUTE:
 
-            # Finding the Links for the Requeted Elemnts
-            bot.scrape()
+            # Finding the Links for the Requested Elements
+            BOT.scrape()
 
             # Using Playwright API to perform Automatic Actions
             with sync_playwright() as p:
 
                 # Creating a Browser Instance
-                browser = p.chromium.launch(headless=True)
+                browser = p.chromium.launch(headless=False)
                 page = browser.new_page()
 
-                # Adding the Requeted Elements to the Basket
-                bot.addTobasket(page)
+                # Adding the Requested Elements to the Cart
+                BOT.add_to_basket(page)
 
                 # Checkout
-                bot.checkout(page)
+                BOT.checkout(page)
 
                 # Exit the Script
                 exit()
