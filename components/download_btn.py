3d1ccea926e651fe3acbdb41665bcea9cf220f3d
francesco <francescosacco.github@gmail.com>
2023-12-24 17:24:18 +0100

    Refactor code splitting and decorator

--- components/download_btn.py
+++ components/download_btn.py
@@ -4,10 +4,10 @@ from components.utils import save_pay_data
 from components.checkout import CheckoutUI
 import json
 import os
+from typing import Dict
 
 class DownloadButtonUI:
     def __init__(self, form: CheckoutUI) -> None:
-
         # Get the absolute path to the JSON file
         json_file_path: str = os.path.join("config", "pay.json")
 
@@ -17,6 +17,15 @@ class DownloadButtonUI:
             type="secondary",
             use_container_width=True)
         if self.download_btn:
-            with open(json_file_path, "w") as f:
-                f.write(f"[{json.dumps(save_pay_data(form), indent=4)}]")
-            st.toast("Payment configuration saved", icon="✅")
+            try:
+                self.save_payment_data(json_file_path, form)
+                st.toast("Payment configuration saved", icon="✅")
+            except Exception as e:
+                st.toast(f"Error saving payment configuration: {e}", icon="❌")
+
+    @staticmethod
+    def save_payment_data(file_path: str, form: CheckoutUI) -> None:
+        # Serialize payment data to JSON and save to file
+        payment_data: Dict[str, str] = save_pay_data(form)
+        with open(file_path, "w") as f:
+            json.dump(payment_data, f, indent=4)
