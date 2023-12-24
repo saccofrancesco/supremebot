624f8b41462ec0ab80026aa99c90602ab98549ea
francesco <francescosacco.github@gmail.com>
2023-12-24 17:11:06 +0100

    Made the categories section more modular

--- components/categories.py
+++ components/categories.py
@@ -1,14 +1,23 @@
 # Importing Libraries
+from typing import List
 from streamlit_option_menu import option_menu
 
 class CategoriesUI:
-    def __init__(self) -> None:
+    # Define a list of available categories
+    CATEGORIES: List[str] = [
+        "T-Shirts", "Accessories", "Sweatshirts", "Hats", "Jackets",
+        "Tops", "Pants", "Skate", "Bags", "Shirts"
+    ]
 
+    def __init__(self) -> None:
         # Input for the user to select item types
-        self.items_category: str = option_menu(
+        self.items_category: str = self.create_category_menu()
+
+    def create_category_menu(self) -> str:
+        # Create a dropdown menu for selecting a category
+        return option_menu(
             menu_title="Select a Category",
-            options=[" T-Shirts", "Accessories", "Sweatshirts", "Hats", "Jackets",
-                     "Tops", "Pants", "Skate", "Bags", "Shirts"],
+            options=self.CATEGORIES,
             default_index=0,
             menu_icon="list-task",
             icons=None,
